821508ca2d1f80ba39042bf684600d7128e021c7
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2022-05-09 18:00:54 +0200

    first commit

--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,8 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
+  app.get('/api/convert?',(req,res)=>{
 
+    res.send(req.query.input)
+  })
 };
--- server.js
+++ server.js
@@ -9,9 +9,9 @@ require('dotenv').config();
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
-
+const morgan = require('morgan')
 let app = express();
-
+app.use(morgan("common"));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
